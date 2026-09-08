6e1309052b1b4bec1d8ade21a8a2370b86be70b9
aliyyulmunif <aliyyulmunif780@gmail.com>
2026-09-08 08:52:51 +0700

    Disable index sitemap generation
    Disable index sitemap generation.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   siteUrl: "https://alymnf.my.id",
   generateRobotsTxt: true,
+  generateIndexSitemap: false, // ini yang bikin dia skip bikin index, langsung 1 file sitemap.xml
 };
